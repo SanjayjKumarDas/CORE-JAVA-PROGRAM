1c4f4d27d0fbda73afa9e18ff3029803d22af1ef
Sanjay Kumar Das <43781122+SanjayjKumarDas@users.noreply.github.com>
2019-07-17 21:17:50 +0530

    Demonstration of Var Concept in Java

---
+++ Var Concept In Java/src/varconcept/VarConceptInJava.java
@@ -0,0 +1,24 @@
+package varconcept;
+
+/**
+ * Date-16/07/2019
+ * 
+ * @author SANJAY
+ *
+ *         VAR is the alternative use of DataType Java Decompiler is need to
+ *         check the VAR
+ */
+public class VarConceptInJava {
+
+	public void varDemo() {
+		var a = 10;
+		var b = 'A';
+		var d = "Sanjay";
+		var e = 25.4;
+	}
+
+	public static void main(String[] args) {
+		VarConceptInJava ob = new VarConceptInJava();
+		ob.varDemo();
+	}
+}
